7704dbc580f395b72d2ffa302ec5b59c25b89cb0
Renan Pupin <renan.pupin@gmail.com>
2018-03-19 12:35:47 -0300

    work at chat

--- App.js
+++ App.js
@@ -4,55 +4,88 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {GiftedChat} from 'react-native-gifted-chat'
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            messages: [
+                {
+                    _id: new Date().getTime(),
+                    text: 'Hello World RN DevPP!',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: require("./logo.png"),
+                    },
+                },
+            ],
+        }
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+
+        setTimeout(() => {
+            this.setState(previousState => ({
+                messages: GiftedChat.append(previousState.messages, {
+                    _id: new Date().getTime(),
+                    text: 'Hello DevPP!',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: require("./logo.png"),
+                    },
+                }),
+            }));
+        }, 1500)
+    }
+
+    render() {
+        return (
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                placeholder={"Escreva uma mensagem"}
+                user={{
+                    _id: 1,
+                }}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
---
+++ README.md
@@ -0,0 +1,27 @@
+
+# rn-meetup
+React Native project created in the first DevPP meetup.
+
+## Getting started
+```bash
+$ git clone https://github.com/renanpupin/rn-meetup
+$ cd rn-meetup
+```
+
+## Install
+```bash
+$ yarn
+
+#or
+
+$ npm install
+```
+
+## Run
+```bash
+$ react-native run-ios
+
+#or
+
+$ react-native run-android
+```
